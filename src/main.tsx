f174a1d442952471f6b7db19502c8dd368fef271
Naomi <gabriella.naomi21@ui.ac.id>
2025-03-07 10:10:17 +0700

    chore: setup jest

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
-import App from './App.tsx';
-import { DesignSystemModule } from './modules/DesignSystemModule/index.tsx';
+import App from "./App"
+import { DesignSystemModule } from './modules/DesignSystemModule';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
