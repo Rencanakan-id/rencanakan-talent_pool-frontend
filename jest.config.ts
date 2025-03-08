783cd61a1dfa80e7fd1e7071a76c6e787c2ae0dc
Abbilville <abbilville@gmail.com>
2025-03-09 01:29:47 +0700

    chore: setup jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,12 +6,15 @@ const config: Config = {
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Biar Jest bisa handle import CSS
+    '^@/components(.*)$': '<rootDir>/src/components$1', // Tambahkan alias untuk @/components
+    '^@/modules(.*)$': '<rootDir>/src/modules$1', // Tambahkan alias untuk @/modules
+    '^@/lib(.*)$': '<rootDir>/src/lib$1', // Tambahkan alias untuk @/lib
+    '^@/data(.*)$': '<rootDir>/src/data$1', // Tambahkan alias untuk @/data
   },
   collectCoverage: true, // Mengaktifkan coverage
   coverageDirectory: 'coverage', // Direktori hasil laporan
   collectCoverageFrom: ['src/**/*.{ts,tsx}'], // File yang ingin diukur coverage-nya
   coverageReporters: ['json', 'lcov', 'text', 'clover'], // Format laporan coverage
-  
 };
 
 export default config;
