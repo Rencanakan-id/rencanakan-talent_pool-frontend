a2a8673266df7433af850bf4dd2e1ea050e702a5
Naomi <gabriella.naomi21@ui.ac.id>
2025-03-08 14:46:20 +0700

    [REFACTOR] make module for test

--- src/__tests__/pereviewTalent/experience.test.tsx
+++ src/__tests__/pereviewTalent/experience.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Experience from '../components/ui/experience';
+import Experience from '../../components/ui/experience';
 
 // Mock Typography agar tidak perlu bergantung pada library lain
-jest.mock('../components/atoms/typography', () => ({
+jest.mock('../../components/atoms/typography', () => ({
   Typography: ({ variant, className, children }: { variant: string, className?: string, children: React.ReactNode }) => (
     <p data-testid={variant} className={className}>{children}</p>
   ),
