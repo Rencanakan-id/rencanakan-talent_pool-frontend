b15198b1e06cbf5a41af141c0ccd0ae4f6f6431b
meteoreinger <reikathr@gmail.com>
2025-03-09 19:59:40 +0700

    [RED] create skeleton for stepthreeform

---
+++ src/modules/RegisterFormModule/Section/register-3.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface StepThreeFormProps {
+  formData: any;
+  updateFormData: (data: any) => void;
+}
+
+export const StepThreeForm: React.FC<StepThreeFormProps> = () => {
+  return <div></div>;
+};
