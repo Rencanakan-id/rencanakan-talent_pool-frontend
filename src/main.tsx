18fb9041bff90d77d7164d9f985143e1198450d3
raviehasan <ravie.hasan@ui.ac.id>
2025-03-11 16:08:11 +0700

    chore: changed dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/design" element={<DesignSystemModule />} />
         <Route path="/register" element={<RegisterModule />} />
         <Route path="/login" element={<LoginModule />} />
-        <Route path="/register" element={<RegisterModule />} />
       </Route>
     </Routes>
   </BrowserRouter>
