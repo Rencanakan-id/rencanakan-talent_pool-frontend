9b07f9baaeace91fa3de25e73c9234f871d2d3b6
William <williamsalimrjs@gmail.com>
2025-03-13 10:00:52 +0700

    add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router';
 import { Navbar } from './components/layout/navbar';
 import { Footer } from './components/layout/footer';
+import { AuthContextProvider } from './components/context/authContext';
+import Cookies from 'js-cookie';
 
 function App() {
+  const initialToken = Cookies.get('jwt') || ''; 
   return (
+    <AuthContextProvider initialToken={initialToken}> 
     <div>
       <Navbar />
       <div className="min-h-[calc(100vh-80px)]">
@@ -11,6 +15,7 @@ function App() {
       </div>
       <Footer />
     </div>
+    </AuthContextProvider> 
   );
 }
 
