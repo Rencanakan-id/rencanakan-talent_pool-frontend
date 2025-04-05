fc933410c17d955bb9911dd1cf57012405ba1f44
Scarletra <fernandovalentino10@gmail.com>
2025-04-05 12:39:14 +0700

    [GREEN] Implement preview if there is any certification (positive case)

--- src/components/ui/certificate.tsx
+++ src/components/ui/certificate.tsx
@@ -22,7 +22,7 @@ const Certificate: React.FC<CertificateProps> = ({ certificates = [] }) => {
         
         return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
     };
-    
+
     return (
         <div className="min-h-[200px] w-full rounded-[8px] border border-gray-300 px-6 py-6">
             <Typography variant="p1" className="pb-4 text-rencanakan-type-black">
@@ -32,21 +32,18 @@ const Certificate: React.FC<CertificateProps> = ({ certificates = [] }) => {
             {certificates.length > 0 ? (
                 <div className="w-full space-y-2 divide-y divide-gray-300">
                     {certificates.map((cert) => (
-                        <div key={cert.id} className="min-h-[112px] space-y-1 flex">
+                        <div key={cert.id} className="min-h-18 space-y-1 flex">
                             <img src="/pdf.svg" alt="Logo" className="h-[40px] w-[40px]" />
                             <div>
-                                <Typography variant="h5"> {cert.title} </Typography>
-                                <Typography variant="p3">
-                                    {' '}
-                                    {cert.file.name}{' '}
-                                </Typography>
+                                <Typography variant="h5" className="text-xs text-rencanakan-type-black"> {cert.file.name} </Typography>
+                                <Typography variant="p3">{formatFileSize(cert.file.size)}</Typography>
                             </div>
                         </div>
                     ))}
                 </div>
             ) : (
                 <Typography variant="p3" className="text-gray-500">
-                    Tidak ada sertifikat.
+                    Tidak ada sertifikasi.
                 </Typography>
             )}
         </div>
