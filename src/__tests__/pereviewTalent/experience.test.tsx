ea291005068630e2355fad65d96e7cfe6603458f
Scarletra <fernandovalentino10@gmail.com>
2025-04-05 16:07:02 +0700

    fix: Fix build error bcs of testing issue

--- src/__tests__/pereviewTalent/experience.test.tsx
+++ src/__tests__/pereviewTalent/experience.test.tsx
@@ -23,7 +23,7 @@ describe('Experience Component', () => {
     expect(screen.getByText('Pengalaman')).toBeInTheDocument();
     expect(screen.getByText('Software Engineer')).toBeInTheDocument();
     expect(screen.getByText('Tech Corp • FULL_TIME')).toBeInTheDocument();
-    expect(screen.getByText('Jakarta, Indonesia - 2023-08-31')).toBeInTheDocument();
+    expect(screen.getByText('Jakarta, Indonesia')).toBeInTheDocument();
   });
 
   test('renders message when there is no data', () => {
